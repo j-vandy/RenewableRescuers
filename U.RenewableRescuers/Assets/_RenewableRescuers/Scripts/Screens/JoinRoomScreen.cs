8223fc12b4a1b40d8de85fdc9c07e12509563bdb
JacksonV <jacksonvan2003@gmail.com>
2024-03-04 22:42:20 -0600

    Added most of the functionality between the startup / lobby screens & added tmp

---
+++ U.RenewableRescuers/Assets/_RenewableRescuers/Scripts/Screens/JoinRoomScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinRoomScreen : Screen
+{
+    void Awake()
+    {
+    }
+
+    public void CreateBttnClicked()
+    {
+        //PhotonManager.Instance.CreateRoom(createInputField.text);
+    }
+
+    public void JoinBttnClicked()
+    {
+        //PhotonManager.Instance.JoinRoom(joinInputField.text);
+    }
+}
